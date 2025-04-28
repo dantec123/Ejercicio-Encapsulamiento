bd588228a883b747efd0ed6e2d9b1a36b63c201d
dantec123 <dante.corzo.081@alu.tecnica29de6.edu.ar>
2025-04-28 08:54:56 -0300

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,12 +1,32 @@
+
 class Coche:
     def __init__(self, marca, modelo, año):
-        self.marca = marca
-        self.modelo = modelo
-        self.año = año
+        self._marca = marca
+        self._modelo = modelo
+        self._año = año
 
     def mostrar_informacion(self):
-        return f"Coche: {self.marca} {self.modelo}, Año: {self.año}"
+        return f"Coche: {self._marca} {self._modelo}, Año: {self._año}"
 
-if __name__ == "__main__":
-    coche1 = Coche("Toyota", "Corolla", 2020)
-    print(coche1.mostrar_informacion())
+    def get_marca(self):
+        return self._marca
+
+    def set_marca(self, marca):
+        self._marca = marca
+
+    def get_modelo(self):
+        return self._modelo
+
+    def set_modelo(self, modelo):
+        self._modelo = modelo
+
+    def get_año(self):
+        return self._año
+
+    def set_año(self, año):
+        if año > 1885:  # El primer coche fue inventado en 1886
+            self._año = año
+        else:
+            print("Año no válido. El año debe ser mayor que 1885.")
+
+
