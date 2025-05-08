b2fc849a9e61106695764ba814997fb058a19c74
dantec123 <dante.corzo.081@alu.tecnica29de6.edu.ar>
2025-05-07 22:00:34 -0300

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,34 +1,23 @@
-class persona:
-    def __init__(self,nombre,edad,sexo):
-        self.nombre=nombre
-        self.edad=edad
-        self.sexo=sexo
-    
-    def mostrar_info(self):
-        return f"Nombre: {self.nombre}, Edad: {self.edad}, Sexo: {self.sexo}"
-    
-    def get_nombre(self):
-        return self.nombre
+class Persona:
+    def __init__(self, nombre, edad, sexo):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
 
-    def set_nombre(self,nombre):
-        self.nombre=nombre
-    
-    def get_edad(self):
-        return self.edad
-    
-    def set_edad(self,edad):
-        self.edad=edad
-        
-    def get_sexo(self):
-        return self.sexo
-    
-    def set_sexo(self,sexo):
-        self.sexo=sexo
-    
-    def cambiar_edad(self):
-        
-    
-if __name__=="__main__":
-    persona1=persona("Gabriel",17,"Masculino")
-    print(persona1.mostrar_info())
+    def cambiar_edad(self, nueva_edad):
+        self.edad = nueva_edad
+
+    def mostrar_informacion(self):
+        print("Nombre:", self.nombre)
+        print("Edad:", self.edad)
+        print("Sexo:", self.sexo)
+
+if __name__ == "__main__":
+    persona1 = Persona("Dante", 17, "Masculino")
+    persona1.mostrar_informacion()
+
+    print("\nCambiando edad...\n")
+    persona1.cambiar_edad(30)
+    persona1.mostrar_informacion()
+
         
